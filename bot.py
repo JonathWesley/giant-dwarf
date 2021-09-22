5bcb2e935baca02a3dae50d942603c8e1e5a53c5
JonathWesley <jonathhherdt@gmail.com>
2021-09-22 14:33:46 -0300

    add one way to do music

--- bot.py
+++ bot.py
@@ -1,35 +1,26 @@
 # bot.py
 import os
 import random
-
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+
+#import all of the cogs
+from main_cog import main_cog
+from image_cog import image_cog
+from music_cog import music_cog
+
+bot = commands.Bot(command_prefix='/')
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='!')
+#remove the default help command so that we can write out own
+bot.remove_command('help')
 
-@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
-
-@bot.command(name='roll_dice', help='Simulates rolling dice.')
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
-    dice = [
-        str(random.choice(range(1, number_of_sides + 1)))
-        for _ in range(number_of_dice)
-    ]
-    await ctx.send(', '.join(dice))
+#register the class with the bot
+bot.add_cog(main_cog(bot))
+bot.add_cog(image_cog(bot))
+bot.add_cog(music_cog(bot))
 
 bot.run(TOKEN)
